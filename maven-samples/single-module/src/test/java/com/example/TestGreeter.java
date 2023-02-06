9c1c846238bb1e55b44af389e2d5ab7b888a09cf
saddam40500 <saddam40500@gmail.com>
2023-02-05 21:43:39 -0600

    maven jenkins job hrough poling

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -33,7 +33,7 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you making me Happy";
+    String someone = "Saddam Changes";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
